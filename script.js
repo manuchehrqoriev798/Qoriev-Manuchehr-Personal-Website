c6336f25acabc22bf06f1fdd44253ca82ab23ce0
manuchehrqoriev798 <manuchehrqoriev798@gmail.com>
2026-01-04 02:57:26 +0600

    Remove progress indicator and associated styles and scripts to streamline the user interface

--- script.js
+++ script.js
@@ -69,30 +69,6 @@ var swiper = new Swiper(".mySwiper", {
     }
   });
 
-// side progress bar 
-
-let calcScrollValue = ()=>{
-    let scrollProgress = document.getElementById("progress");
-    let pos = document.documentElement.scrollTop;
-
-    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    let scrollValue = Math.round((pos * 100)/calcHeight);
-    
-    if(pos > 100){
-        scrollProgress.style.display = "grid";
-    }else{
-        scrollProgress.style.display = "none";
-    }
-
-    scrollProgress.addEventListener("click",()=>{
-        document.documentElement.scrollTop = 0;
-    });
-
-    scrollProgress.style.background = `conic-gradient(#fff ${scrollValue}%,#e6006d ${scrollValue}%)`;
-};
-
-window.onscroll = calcScrollValue;
-window.onload = calcScrollValue;
 
 
 // active menu 
